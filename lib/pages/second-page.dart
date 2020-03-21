30a4b725bb0f861004ae165df07138602007b6a5
Simon Uhrmann <suhrmann@posteo.de>
2020-03-21 21:23:21 +0100

    Add notification test

--- lib/pages/second-page.dart
+++ lib/pages/second-page.dart
@@ -3,29 +3,51 @@ import 'package:scoped_model/scoped_model.dart';
 import '../scoped-model/main-model.dart';
 
 class SecondPage extends StatelessWidget {
+
+  Future<void> _showNotification() async {
+    print('Test: Notification. . . ');
+  }
+
   @override
   Widget build(BuildContext context) {
     return ScopedModelDescendant<MainModel>(
         builder: (BuildContext context, Widget child, MainModel model) {
       return Container(
         child: 1 == 1
-            ? Row(
-                children: <Widget>[
-                  Text("Dies ist die zweite Seite!"),
-                  RaisedButton(
-                    child: Text("Play Sound"),
-                    onPressed: () {
-                      model.play("haendewaschsongORF.mp3");
-                    },
-                  ),
-                  RaisedButton(
-                    child: Text("Stop Sound"),
-                    onPressed: () {
-                      model.stop();
-                    },
-                  ),
-                ],
-              )
+          ? Column(
+          children: <Widget>[
+            Text("Dies ist die zweite Seite!"),
+
+            Row(
+              children: <Widget>[
+                RaisedButton(
+                  child: Text("Play Sound"),
+                  onPressed: () {
+                    model.play("haendewaschsongORF.mp3");
+                  },
+                ),
+                RaisedButton(
+                  child: Text("Stop Sound"),
+                  onPressed: () {
+                    model.stop();
+                  },
+                ),
+              ],
+            ),
+
+            Row(
+              children: <Widget>[
+                RaisedButton(
+                  child: Text("Test Notification"),
+                  onPressed: () {
+                    _showNotification();
+                  },
+                ),
+              ],
+            ),
+          ],
+        )
+
             : Container(),
       );
     });
