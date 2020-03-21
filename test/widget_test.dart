c19e1b1ad9170d77c335deae5e540093997b42eb
Tobias Luderer <tobias.luderer@gmail.com>
2020-03-21 11:41:45 +0100

    boilerplate ausgedünnt

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:ich_mache_es_richtig_richtig_oder/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(IchMacheEsRichtigODER());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
