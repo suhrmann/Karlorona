2e2faaea9e356926d86d6fa24e1e303d442eb952
marcellus1978 <47270801+marcellus1978@users.noreply.github.com>
2020-04-19 14:09:23 +0200

    update Info Hand Pages

--- lib/activities/Info/info-hand-page.dart
+++ lib/activities/Info/info-hand-page.dart
@@ -12,6 +12,31 @@ class InfoHandPage extends StatelessWidget {
             ActivityInfotext(
               text: "Schmutz und auch Krankheitskeime abwaschen – das klingt einfach. Richtiges Händewaschen erfordert aber ein sorgfältiges Vorgehen. Häufig werden die Hände beispielsweise nicht ausreichend lange eingeseift und insbesondere Handrücken, Daumen und Fingerspitzen vernachlässigt. Wasche deine Hände am besten so:",
             ),
+            ListTile(
+              leading: Image.asset("assets/images/icons/haendewaschen_schritt1.jpg"),
+              title: Text('Halten Sie die Hände zunächst unter fließendes Wasser. Die Temperatur können Sie so wählen, dass sie angenehm ist.',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal),),
+            ),
+            ListTile(
+              leading: Image.asset("assets/images/icons/haendewaschen_schritt2.jpg"),
+              title: Text('Seifen Sie dann die Hände gründlich ein – sowohl Handinnenflächen als auch Handrücken, Fingerspitzen, Fingerzwischenräume und Daumen. Denken Sie auch an die Fingernägel. Hygienischer als Seifenstücke sind Flüssigseifen, besonders in öffentlichen Waschräumen.',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal),),
+            ),
+            ListTile(
+              leading: Image.asset("assets/images/icons/haendewaschen_schritt3.jpg"),
+              title: Text('Reiben Sie die Seife an allen Stellen sanft ein. Gründliches Händewaschen dauert 20 bis 30 Sekunden.',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal),),
+            ),
+            ListTile(
+              leading: Image.asset("assets/images/icons/haendewaschen_schritt4.jpg"),
+              title: Text('Danach die Hände unter fließendem Wasser abspülen. Verwenden Sie in öffentlichen Toiletten zum Schließen des Wasserhahns ein Einweghandtuch oder Ihren Ellenbogen.',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal),),
+            ),
+            ListTile(
+              leading: Image.asset("assets/images/icons/haendewaschen_schritt5.jpg"),
+              title: Text('Trocknen Sie anschließend die Hände sorgfältig ab, auch in den Fingerzwischenräumen. In öffentlichen Toiletten eignen sich hierfür am besten Einmalhandtücher. Zu Hause sollte jeder sein persönliches Handtuch benutzen.',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal),),
+            ),
           ],
         ),
       ),
